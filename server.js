af2115a7c7ba1e4d281db875acbe3b0ffe4d87fd
Omri <omriluz1994@gmail.com>
2022-06-07 08:21:06 +0300

    .

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const http = require('http').createServer(app)
 
 // Express App Config
 app.use(cookieParser())
-app.use(express.json())
+app.use(express.json({limit: '50mb'}))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
@@ -32,6 +32,7 @@ const {setupSocketAPI} = require('./services/socket.service')
 
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
+// app.use(express.json({limit: '50mb'}));
 // app.use('/api/review', reviewRoutes)
 app.use('/api/board', boardRoutes)
 setupSocketAPI(http)
