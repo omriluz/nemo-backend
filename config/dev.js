41483824e414fa597f688b055315991fadec20ad
Omri <omriluz1994@gmail.com>
2022-06-07 10:51:27 +0300

    .

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb://localhost:27017',
-  // 'dbURL': 'mongodb+srv://yonatanbz:SVp1ST6rQMtkdsBD@cluster0.tm2tn.mongodb.net/?retryWrites=true&w=majority',
+  // dbURL: 'mongodb://localhost:27017',
+  'dbURL': 'mongodb+srv://yonatanbz:SVp1ST6rQMtkdsBD@cluster0.tm2tn.mongodb.net/?retryWrites=true&w=majority',
 }
