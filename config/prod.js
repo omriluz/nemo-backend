35f31cb6aeebd8d1742c071fd99d458f3c4a822d
Omri <omriluz1994@gmail.com>
2022-06-07 08:31:28 +0300

    .

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: 'mongodb+srv://yonatanbz:SVp1ST6rQMtkdsBD@cluster0.tm2tn.mongodb.net/?retryWrites=true&w=majority',
 }
